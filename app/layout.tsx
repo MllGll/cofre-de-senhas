a45be22f899ca54d3f1dfac53c48f07d9f9f6590
MllGll <marcellogallante@gmail.com>
2025-06-23 23:52:47 -0300

    chore: Adiciona suporte para Vercel Analytics e Speed Insights no layout da aplicação

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Toaster } from "sonner";
 
 export const metadata: Metadata = {
@@ -20,6 +22,8 @@ export default function RootLayout({
 			<body>
 				{children}
 				<Toaster richColors position="top-center" />
+				<Analytics />
+				<SpeedInsights />
 			</body>
 		</html>
 	);
